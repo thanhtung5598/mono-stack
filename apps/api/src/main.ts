ead266d50b4a349423f2e577e22d69d1b070b7c1
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-11-20 09:58:04 +0700

    feat(BE): update docker file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
 
   SwaggerModule.setup(`api/docs`, app, document);
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.API_PORT ?? 3001);
 }
 bootstrap();
