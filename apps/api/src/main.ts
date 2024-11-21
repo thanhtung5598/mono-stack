a583b1e95ac461275bd32e1a774c16334a2647e9
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-11-21 16:35:28 +0700

    feat

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     .setTitle('Mono Stack')
     .addBearerAuth()
     .build();
-  console.log('test');
+  console.log('test mew');
 
   const document = SwaggerModule.createDocument(app, config);
 
