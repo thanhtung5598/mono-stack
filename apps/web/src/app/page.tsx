ff03fa9f335ec4eeb6c1d31a514107b7a510c0f4
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-11-25 10:41:25 +0700

    feat

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read our
           </a>
         </div>
       </main>
