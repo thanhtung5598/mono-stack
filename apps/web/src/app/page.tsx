f9c5822a1dd2eb6f5eae235e68ace5b769346608
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-12-04 15:21:26 +0700

    bew

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="text-center">The best 1</div>
-        <div className="text-center">The best 2</div>
-        <div className="text-center">The best 3</div>
-        <div className="text-center">The best 4</div>
-        <div className="text-center">The best 5</div>
+        <div className="text-center">The Mew 1</div>
+        <div className="text-center">The Mew 2</div>
+        <div className="text-center">The Mew 3</div>
+        <div className="text-center">The Mew 4</div>
+        <div className="text-center">The Mew 5</div>
       </main>
     </div>
   );
