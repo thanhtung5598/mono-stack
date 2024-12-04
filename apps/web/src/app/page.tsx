b53be40a24feb27164b7f58c3193e690cc73009e
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-12-04 15:51:55 +0700

    nua nek

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-center">The Mew 1</div>
+        <div className="text-center">The Mew 2</div>
       </main>
     </div>
   );
