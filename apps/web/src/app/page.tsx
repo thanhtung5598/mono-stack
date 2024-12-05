2a1626387ff933351bcf12927d2269516cec23ab
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-12-05 09:48:43 +0700

    Berk

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -2,10 +2,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="text-center">The Mew 1</div>
-        <div className="text-center">The Mew 2</div>
-        <div className="text-center">The Mew 3</div>
-        <div className="text-center">The Mew 4</div>
+        <div className="text-center">Berk 1</div>
+        <div className="text-center">Berk 2</div>
+        <div className="text-center">Berk 3</div>
+        <div className="text-center">Berk 4</div>
+        <div className="text-center">Berk 5</div>
+        <div className="text-center">Berk 6</div>
       </main>
     </div>
   );
