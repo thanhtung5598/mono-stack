c9806e360f172d3f5301efb70cd9c87da56998d0
tunglethanh1998 <tung_le_thanh@bita.jp>
2024-12-09 11:57:46 +0700

    bell 3

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="text-center">Bell 1</div>
         <div className="text-center">Bell 2</div>
+        <div className="text-center">Bell 3</div>
         <div className="text-center">Want testing</div>
       </main>
     </div>
